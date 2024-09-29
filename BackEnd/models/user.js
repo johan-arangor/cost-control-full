890c1e22d57c6945a5a57ab53aac60e405a3fa9a
Johan Arango <johan.arango@experimentality.co>
2024-09-29 14:20:55 -0500

    update user model
    update user model add a associations

--- BackEnd/models/user.js
+++ BackEnd/models/user.js
@@ -6,13 +6,11 @@ const {
 
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      User.hasMany(models.Credit, { foreignKey: 'userId'});
+      User.hasMany(models.Income, { foreignKey: 'userId'});
+      User.hasMany(models.Expense, { foreignKey: 'userId'});
+      User.hasMany(models.Vehicule, { foreignKey: 'userId'});
     }
   }
 
