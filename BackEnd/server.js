08248c7cbbdf37ffeee62ab9906a01df3e530c39
Johan Arango <johan.arango@experimentality.co>
2024-09-28 23:15:05 -0500

    update server file
    update server file config to initialize sequelize orm

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,10 +1,14 @@
+require('dotenv').config();
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
-require('dotenv').config();
-const {PORT_BACK, VERSION_API} = process.env;
-const PORT = process.env.PORT || PORT_BACK;
+const { PORT_BACK, VERSION_API } = process.env;
+const { sequelize } = require('./models/index');
+
+//Config Morgan
+app.use(morgan('dev'));
 
 //Config Apis
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -14,10 +18,20 @@ app.use(bodyParser.json());
 app.use(cors({origin:true}))
 
 //routers
-const routesIndex = require('./routes/index.routes');
-app.use(`/api/${VERSION_API}`, routesIndex);
+try {
+    const routesIndex = require('./routes/index.routes');
+    app.use(`/api/${VERSION_API}`, routesIndex);
+  } catch (error) {
+    console.error('Error al definir las rutas:', error);
+    process.exit(1);
+  }
 
 // Iniciar servidor
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
+app.listen(PORT_BACK, () => {
+  console.log(`Server started on port http://localhost:${PORT_BACK}`);
+
+  sequelize.sync({ force: true })
+  .then(() => {
+    console.log('Se ha establecido la conexión');
+  });
 });
