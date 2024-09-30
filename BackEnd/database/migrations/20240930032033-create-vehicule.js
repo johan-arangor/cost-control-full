7a6247e4c5a6e6e2d09cb46febe7bb167fd498d7
Johan Arango <johan.arango@experimentality.co>
2024-09-29 22:26:06 -0500

    create a model vehicule
    create a new model and migration of vehicule

---
+++ BackEnd/database/migrations/20240930032033-create-vehicule.js
@@ -0,0 +1,46 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Vehicules', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4
+      },
+      plate: {
+        type: Sequelize.STRING,
+        allowNull: fase
+      },
+      make: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      model: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      year: {
+        type: Sequelize.INTEGER,
+        allowNull: true
+      },
+      state: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: true
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Vehicules');
+  }
+};
---
+++ BackEnd/models/vehicule.js
@@ -0,0 +1,49 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Vehicule extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Vehicule.init({
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false
+    },
+    plate: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    make: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    model: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    year: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    state: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true
+    }
+  }, {
+    sequelize,
+    modelName: 'Vehicule',
+  });
+  return Vehicule;
+};
