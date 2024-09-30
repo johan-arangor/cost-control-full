6ad95588db250085e40784762e7e78d7bb138f24
Johan Arango <johan.arango@experimentality.co>
2024-09-30 09:24:18 -0500

    add model payment list
    add a new files model and migrations of payment list

---
+++ BackEnd/models/paymentlist.js
@@ -0,0 +1,52 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class paymentList extends Model {
+    static associate(models) {
+      Credit.belongsTo(models.Credit, { foreignKey: 'credit' });
+    }
+  }
+  
+  paymentList.init({
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false
+    },
+    datePayment: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+    paymentNumber: {
+      type: Sequelize.INTEGER,
+      allowNull: false
+    },
+    amount: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false
+    },
+    amountPaid: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false
+    },
+    datePaymentMade: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+    creditId: {
+      type: DataTypes.UUID,
+      references: {
+        model: 'Credit',
+        key: 'id'
+      }
+    },
+  }, {
+    sequelize,
+    modelName: 'paymentList',
+  });
+
+  return paymentList;
+};
