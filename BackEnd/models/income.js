be483914fb83e92144a39f5723652389926cbf1c
Johan Arango <johan.arango@experimentality.co>
2024-09-29 20:23:29 -0500

    create income model
    add a new model income and migration

---
+++ BackEnd/models/income.js
@@ -0,0 +1,68 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Income extends Model {
+    static associate(models) {
+      Credit.belongsTo(models.User, { foreignKey: 'userId' });
+      Credit.belongsTo(models.Subcategory, { foreignKey: 'subcategoryId' });
+      Credit.belongsTo(models.Vehicule, { foreignKey: 'vehiculeId' });
+      Credit.belongsTo(models.Credit, { foreignKey: 'creditId' });
+    }
+  }
+
+  Income.init({
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false
+    },
+    dateEntry: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    amount: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false
+    },
+    userId: {
+      type: DataTypes.UUID,
+      references: {
+        model: 'User',
+        key: 'id'
+      }
+    },
+    subcategoryId: {
+      type: DataTypes.UUID,
+      references: {
+        model: 'SubCategory',
+        key: 'id'
+      }
+    },
+    vehicule: {
+      type: DataTypes.UUID,
+      references: {
+        model: 'Vehicule',
+        key: 'id'
+      }
+    },
+    creditId: {
+      type: DataTypes.UUID,
+      references: {
+        model: 'Credit',
+        key: 'id'
+      }
+    },
+  }, {
+    sequelize,
+    modelName: 'Income',
+  });
+  
+  return Income;
+};
