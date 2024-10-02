f66be1f9bd5acf197370a276b8050cf61d3cad03
Johan Arango <johan.arango@experimentality.co>
2024-10-01 23:43:51 -0500

    update model user
    update model, and migration of user, add a column password in format string

--- BackEnd/database/seeders/20240929033142-create-users.js
+++ BackEnd/database/seeders/20240929033142-create-users.js
@@ -7,7 +7,7 @@ const moment = require('moment');
 module.exports = {
   async up (queryInterface, Sequelize) {
     let users = [
-      { id: uuidv4(), names: 'Administrator', lastNames: '', email: 'administrator@email.com', createdAt: moment().format("YYYY-MM-DD"), updatedAt: moment().format("YYYY-MM-DD")}
+      { id: uuidv4(), names: 'Administrator', lastNames: '', email: 'administrator@email.com', password: 'asdf23423r4wer25werfsfw45235wfsdf42', createdAt: moment().format("YYYY-MM-DD"), updatedAt: moment().format("YYYY-MM-DD")}
     ];
 
     await queryInterface.bulkInsert('Users', users, {});
--- BackEnd/models/user.js
+++ BackEnd/models/user.js
@@ -32,6 +32,11 @@ module.exports = (sequelize, DataTypes) => {
       allowNull: false,
       unique: true
     },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: false
+    },
     state: {
       type: DataTypes.BOOLEAN,
       allowNull: false,
