f60c0e5d7c544dac55a59385e4ee57c58d15c28a
Johan Arango <johan.arango@experimentality.co>
2024-10-15 21:31:40 -0500

    update homeContoller
    remove console log of controller

--- BackEnd/controllers/home.controller.js
+++ BackEnd/controllers/home.controller.js
@@ -5,23 +5,19 @@ const { User } = require('../models/index');
 
 class HomeController {
   async getIndex(req, res) {
-    try {
-      console.log('aca llega');
-
-      console.log('userData', User);
-      // Lógica para obtener datos de la base de datos
-        //   const data = await db.query('SELECT * FROM tabla');
-        const responseDB = await User.findAll();
-        const result = {
-            title: 'Página de inicio',
-            message: 'Bienvenido a la aplicación',
-            data: responseDB,
-        };
-        
-        return res.status(200).json(result);
+    try 
+    {
+      const responseDB = await User.findAll();
+      const result = {
+          title: 'Página de inicio',
+          message: 'Bienvenido a la aplicación',
+          data: responseDB,
+      };
+      
+      return res.status(200).json(result);
     } catch (error) {
-        const dynamicError = errors.DYNAMIC_ERROR('Error inesperado', `Error al obtener la página de inicio. [ERROR] ${error}`, 'error');
-        return res.status(500).json({ messaje: dynamicError });
+      const dynamicError = errors.DYNAMIC_ERROR('Error inesperado', `Error al obtener la página de inicio. [ERROR] ${error}`, 'error');
+      return res.status(500).json({ messaje: dynamicError });
     }
   }
 }
