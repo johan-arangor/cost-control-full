521b48d0796eb692574c6e99d4ae7965d0509634
Johan Arango <johan.arango@experimentality.co>
2024-10-19 14:38:23 -0500

    Create new model Tag
    add new model tag to data base

---
+++ BackEnd/models/tag.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Tag extends Model {
+    static associate(models) {
+      // Credit.belongsTo(models.Category, { foreignKey: 'categoryId' });
+    }
+  }
+
+  Tag.init({
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    categoryId: {
+      type: DataTypes.UUID,
+      // references: {
+      //   model: 'category',
+      //   key: 'id'
+      // }
+    }
+  }, {
+    sequelize,
+    modelName: 'Tag',
+  });
+
+  return Tag;
+};
