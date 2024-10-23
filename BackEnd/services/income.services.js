982ea10467995abe2dd433ed72dc65a0bcd86563
Johan Arango <johan.arango@experimentality.co>
2024-10-23 01:05:55 -0500

    update services returns
    update services returns with try catch

--- BackEnd/services/income.services.js
+++ BackEnd/services/income.services.js
@@ -1,38 +1,89 @@
+const { Income } = require('../models/index');
+const errors = require('../utils/errors');
+const responses = require('../utils/responses');
+const { v4: uuidv4 } = require('uuid');
+const moment = require("moment/moment");
+
 class IncomeServices {
-    async ValidateIncome(income) {
-        return new Promise(async (resolve) => {
-            resolve('income exist');
-        });
+    async GetIdIncome(incomeId) {
+        try {
+            let result = await Income.findOne(
+                { where: { id: incomeId}}
+            );
+
+            return responses.RESPONSE_DATA(true, result);
+        } catch (err) {
+            throw errors.OPERATIOS_DB.ERROR(err.message);
+        }
     }
 
-    async GetIdIncome(income) {
-        return new Promise(async (resolve) => {
-            resolve('income get id');
-        });
+    async GetAllIncome(tagUserId) {
+        try {
+            let result = await Income.findAll(
+                { where: { userId: tagUserId }}
+            );
+
+            if (result != null) {
+                return responses.RESPONSE_DATA(true, {result});
+            } else {
+                return errors.ERROR_NO_DATA;
+            }
+        } catch (err) {
+            throw errors.OPERATIOS_DB.ERROR;
+        }
     }
 
-    async GetAllIncome(income) {
-        return new Promise(async (resolve) => {
-            resolve('income get all');
-        });
+    async CreateIncome(description, amount, userId, tags, vehiculeId, creditId) {
+        try {
+            await Income.create({
+                id: uuidv4(),
+                dateEntry: moment().format("YYYY-MM-DD"),
+                description: description,
+                amount: amount,
+                userId: userId,
+                tags: tags,
+                vehiculeId: vehiculeId,
+                creditId: creditId
+            });
+
+            return responses.CREATE_DINAMIC('exponse');
+        } catch (err) {
+            throw errors.OPERATIOS_DB.ERROR(err.message);
+        }
     }
 
-    async CreateIncome(income) {
-        return new Promise(async (resolve) => {
-            resolve('income create');
-        });
+    async UpdateIncome(description, amount, userId, tags, vehiculeId, creditId) {
+        try{
+            await Tag.update(
+                { 
+                    id: uuidv4(),
+                    dateEntry: moment().format("YYYY-MM-DD"),
+                    description: description,
+                    amount: amount,
+                    userId: userId,
+                    tags: tags,
+                    vehiculeId: vehiculeId,
+                    creditId: creditId
+                },
+                { where: { id: tagId}}
+            );
+
+            return responses.UPDATE_DINAMIC('tag');
+        } catch (err) {
+            throw errors.OPERATIOS_DB.ERROR(err.message);
+        }
     }
 
-    async UpdateIncome(income) {
-        return new Promise(async (resolve) => {
-            resolve('income update');
-        });
-    }
+    async DeleteIncome(incomeId) {
+        try {
+            await Income.destroy(
+                { where: { id: incomeId}}
+            );
 
-    async DeleteIncome(income) {
-        return new Promise(async (resolve) => {
-            resolve('income delete');
-        });
+            return responses.DELETE_DINAMIC('tag');
+        } catch (err) {
+            throw errors.OPERATIOS_DB.ERROR(err.message);
+        }
     }
 }
 
