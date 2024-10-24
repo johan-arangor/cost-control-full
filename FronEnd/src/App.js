2017862144a8d109dd532ed39a13697076d515e5
Johan Arango <johan.arango@experimentality.co>
2024-10-23 22:55:35 -0500

    update config front
    update config front to render views

--- FronEnd/src/App.js
+++ FronEnd/src/App.js
@@ -3,14 +3,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/layout/layout';
 import Login from './features/login/index';
 import Signup from './features/signup/index';
-import ConfirmAccount from './features/signup/indexConfirmAccount';
-import RenewPassword from './features/login/indexRenewPassword';
-import ChangePassword from './features/login/indexChangePassword';
 import Principal from './features/principal/index';
-import Settings from './features/settings/index';
-import EntitysPrincipal from './features/entitys/index';
-import CategoriesPrincipal from './features/categories/index';
-import SubCategoriesPrincipal from './features/subcategories/index';
+// import ConfirmAccount from './features/signup/indexConfirmAccount';
+// import RenewPassword from './features/login/indexRenewPassword';
+// import ChangePassword from './features/login/indexChangePassword';
+// import Settings from './features/settings/index';
+// import EntitysPrincipal from './features/entitys/index';
+// import CategoriesPrincipal from './features/categories/index';
+// import SubCategoriesPrincipal from './features/subcategories/index';
 
 function App() {
   return (
@@ -21,10 +21,10 @@ function App() {
             <Route exact path="/" element={<Login />} />
             <Route exact path="/login" element={<Login />} />
             <Route exact path="/signup" element={<Signup />} />
+            <Route exact path="/principal" element={<Principal />} />
             {/* <Route exact path="/confirmAccount/:token" element={<ConfirmAccount />} />
             <Route exact path="/renewPassword" element={<RenewPassword />} />
             <Route exact path="/changePassword/:token" element={<ChangePassword />} />
-            <Route exact path="/principal" element={<Principal />} />
             <Route exact path="/settings" element={<Settings />} />
             <Route exact path="/entitys" element={<EntitysPrincipal />} />
             <Route exact path="/categories" element={<CategoriesPrincipal />} />
