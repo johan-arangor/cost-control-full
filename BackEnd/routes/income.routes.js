20f5d4c94336e0699288c368477eb491fc2dbea5
Johan Arango <johan.arango@experimentality.co>
2025-04-23 17:17:15 -0500

    add new route to backend

--- BackEnd/routes/income.routes.js
+++ BackEnd/routes/income.routes.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const IncomeController = require('../controllers/income.controller');
 
-router.post("/income", IncomeController.newIncome);
+router.post("/", IncomeController.newIncome);
 router.get("/incomeAll/:userId", IncomeController.getAllIncomes);
 router.get("/income/:id", IncomeController.getIncomeId);
-router.put("/income", IncomeController.editIncomeId);
-router.delete("/income", IncomeController.deleteIncomeId);
+router.put("/", IncomeController.editIncomeId);
+router.delete("/", IncomeController.deleteIncomeId);
 
 module.exports = router;
--- BackEnd/routes/index.routes.js
+++ BackEnd/routes/index.routes.js
@@ -6,11 +6,13 @@ const userRoutes = require('./user.routes');
 const incomeRoutes = require('./income.routes');
 const expenseRoutes = require('./expense.routes');
 const tagRoutes = require('./tag.routes');
+const vehiculeRoutes = require('./vehicule.routes');
 
 router.use('/', homeRoutes);
 router.use('/user', userRoutes);
 router.use('/income', incomeRoutes);
 router.use('/expense', expenseRoutes);
 router.use('/tag', tagRoutes);
+router.use('/vehicule', vehiculeRoutes);
 
 module.exports = router;
